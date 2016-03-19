0395b22a3b55e8efdafa8db3e7b8cbbbaa00f362
Ryan B <ryan.bliton@gmail.com>
2016-03-19 11:42:49 -0500

    added first two event handlers

--- CarLot.js
+++ CarLot.js
@@ -1,7 +1,8 @@
+"use strict";
 var outputTarget = document.getElementById("outputTarget");
 
 var CarLot = (function () {
-  var inventory = [];
+  let inventory = [];
 
   return {
     getInventory: function () {
@@ -9,23 +10,24 @@ var CarLot = (function () {
     },
 
     //get JSON & parse into an array of objects
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
+    loadInventory: function(callback) {
+      let inventoryLoader = new XMLHttpRequest();
 
       inventoryLoader.open("GET", "inventory.json");
       inventoryLoader.send();
 
       inventoryLoader.addEventListener("load", function () {
         inventory = JSON.parse(this.responseText).cars;
-        callback(inventory);
+        callback(inventory, CarLot.cardClick);
       });
     },
 
+
     // make array of objects into html cards
-    buildCards: function(cars){
-      for (var i=0;i<cars.length;i++) {
-        var current = cars[i];
-        var available = "";
+    buildCards: function(cars, callback){
+      for (let i=0;i<cars.length;i++) {
+        let current = cars[i];
+        let available = "";
         // prepare an availability declaration
         if (current.purchased){
           available = "<p>Not currently available for sale.</p>"
@@ -33,22 +35,29 @@ var CarLot = (function () {
           available = "<p>Available for sale!</p>"
         }
         // build output string
-        var carString = ""
+        let carString = ""
         carString += "<div class='col-xs-4 center'>";
-        carString += "<div class='card'>";
+        carString += "<div class='card' id='card"+i+"'>";
         carString += `<p>Make: ${current.make}<p>`;
         carString += `<p>Model: ${current.model}<p>`;
         carString += `<p>Year: ${current.year}<p>`;
         carString += `<p>Price: ${current.price}<p>`;
         carString += `<p>Color: ${current.color}<p>`;
         carString += available;
-        carString += `<p>About this vehicle: ${current.description}<p>`;
+        carString += `<p class='desc'>About this vehicle: ${current.description}<p>`;
         //create a card with the info from each object
         outputTarget.innerHTML += carString;
+
+        //prep border color
+        let borderColor = "2px solid " + current.color.toLowerCase();
+
+        //style current card with JSON color
+        let currentCard = document.getElementById("card"+i)
+        currentCard.style.border = borderColor;
       }
+    callback();
     }
   }
-
 })();
 
 CarLot.loadInventory(CarLot.buildCards);
--- EventHandlers.js
+++ EventHandlers.js
@@ -1,2 +1,46 @@
-//iife augment for CarLot.js
-  //event handlers
+"use strict";
+let textInput = document.getElementById("textInput")
+let thisDesc = [];
+
+var CarLot = (function(carlot) {
+ let cards = document.getElementsByClassName("card")
+ carlot.cardClick = function(){
+  for (let i=0; i<cards.length;i++){
+    cards[i].addEventListener("click", function(){
+      textInput.focus();
+
+      thisDesc = this.getElementsByClassName("desc");
+
+      //remove selected from all cards
+      for (var j=0;j<cards.length;j++) {
+        cards[j].classList.remove("selected");
+      };
+
+      //add selected to clicked card
+      this.classList.add("selected");
+    });
+  }
+ }
+  return carlot;
+})(CarLot || {});
+
+// "use strict";
+// let thisDesc = [];
+// let textInput = document.getElementById("textInput");
+
+// var CarLot = (function(carlot) {
+//  let cards = document.getElementsByClassName("card");
+//  carlot.cardClick = function(){
+//   for (let i=0; i<cards.length;i++){
+//     cards[i].addEventListener("click", function(){
+
+//     console.log("click");
+
+//     textInput.focus();
+
+//     // get reference to selected bio for editing
+//     thisDesc = this.getElementsByClassName("desc");
+//   });
+//  };
+//   return carlot;
+// })(CarLot || {});
