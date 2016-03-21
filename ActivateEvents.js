912e228a9f625602996e7abafd8dc3ffdbd0874d
Ryan B <ryan.bliton@gmail.com>
2016-03-20 21:25:15 -0500

    moved event handler functionality to activateEvents iife

--- ActivateEvents.js
+++ ActivateEvents.js
@@ -1,4 +1,23 @@
-//iife augment for CarLot.js 
-  //resets card styling to default
+let textInput = document.getElementById("textInput")
+let thisDesc = [];
+var cards = document.getElementsByClassName("card")
 
-  //styles cards- widen border & set new background color. accepts 2 arguments: -car dom element clicked on, color name
+var CarLot = (function(carlot) {
+  carlot.cardSelected = function(){
+    textInput.focus();
+
+    thisDesc = this.getElementsByClassName("desc");
+    // let selectedColor = CarLot.getInventory()[i].color.toLowerCase();
+
+    //remove selected from all cards
+    for (var j=0;j<cards.length;j++) {
+      cards[j].classList.remove("selected");
+      cards[j].style.border = "2px solid " + CarLot.getInventory()[j].color.toLowerCase();
+    };
+
+    //add selected to clicked card
+    this.classList.add("selected");
+    this.style.border = "8px solid black"//+ selectedColor;
+  }
+  return carlot;
+})(CarLot || {});
--- EventHandlers.js
+++ EventHandlers.js
@@ -1,58 +1,20 @@
 "use strict";
 let textInput = document.getElementById("textInput")
 let thisDesc = [];
+var cards = document.getElementsByClassName("card")
 
 var CarLot = (function(carlot) {
- let cards = document.getElementsByClassName("card")
- carlot.activateEvents = function(){
-  for (let i=0; i<cards.length;i++){
-    cards[i].addEventListener("click", function(){
-      textInput.focus();
+  carlot.activateEvents = function(){
+    for (let i=0; i<cards.length;i++){
+      cards[i].addEventListener("click", CarLot.cardSelected);
 
-      thisDesc = this.getElementsByClassName("desc");
-      let selectedColor = CarLot.getInventory()[i].color.toLowerCase();
-
-      //remove selected from all cards
-      for (var j=0;j<cards.length;j++) {
-        cards[j].classList.remove("selected");
-        cards[j].style.border = "2px solid " + CarLot.getInventory()[j].color.toLowerCase();
-      };
-
-      //add selected to clicked card
-      this.classList.add("selected");
-      this.style.border = "8px solid "+ selectedColor;
-    });
-
-  textInput.addEventListener("keyup", function(e){
-  thisDesc[0].innerHTML = textInput.value;
-  console.log("e", e.code);
-  if (e.code === "Enter") {
-    inputField.value = "";
+      textInput.addEventListener("keyup", function(e){
+        thisDesc[0].innerHTML = textInput.value;
+        if (e.code === "Enter") {
+          inputField.value = "";
+        }
+      })
+    }
   }
-})
-
-  }
- }
   return carlot;
 })(CarLot || {});
-
-// "use strict";
-// let thisDesc = [];
-// let textInput = document.getElementById("textInput");
-
-// var CarLot = (function(carlot) {
-//  let cards = document.getElementsByClassName("card");
-//  carlot.cardClick = function(){
-//   for (let i=0; i<cards.length;i++){
-//     cards[i].addEventListener("click", function(){
-
-//     console.log("click");
-
-//     textInput.focus();
-
-//     // get reference to selected bio for editing
-//     thisDesc = this.getElementsByClassName("desc");
-//   });
-//  };
-//   return carlot;
-// })(CarLot || {});
